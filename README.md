737057269cdd04b24eb4ac4184d74cb6a9ab8cd1
Inflex <pldaniels@gmail.com>
2016-09-27 02:24:10 +1000

    Added -p to show paths being traversed via stderr

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # md5walker
 Creates a MD5 hash list of a filesystem tree.  This is meant as a simple tool for use with ddrescue when trying to find damaged files.  
+
+Previously you could do this with a simple find call but it would tend to hang or cause issues with unpredictably damaged files and/or filesystems.
+
+md5walker will try to stop and/or filter the hiccups.
+
+
+##Usage
+
+$ ./md5walker ./path-to-filesystem 
+
--- md5walker.c
+++ md5walker.c
@@ -7,6 +7,15 @@
 
 #include "md5.h"
 
+struct globals {
+	int debug;
+	int verbose;
+	int showdirs;
+	char *inputpath;
+};
+
+struct globals *glb;
+
 static int md5test( const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf ) {
 
 	if (typeflag == FTW_F) {
@@ -16,19 +25,57 @@ static int md5test( const char *fpath, const struct stat *sb, int typeflag, stru
 		if (rr == 0) fprintf(stdout,"%s %s\n", fpath, hash );
 	}
 
+	if ((typeflag == FTW_D)&&(glb->showdirs)) {
+		fprintf(stderr,"%s\n",fpath);
+	}
+
 	return 0;
 
+}
+
+int parse_parameters( struct globals *g, int argc, char **argv ) {
+
+	int i;
+
+	for (i = 0; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			switch (argv[i][1]) {
+//				case 'h': fprintf(stdout,"%s", help); exit(0); break;
+//				case 'V': fprintf(stdout,"%s\n",  VERSION); exit(0); break;
+				case 'v': (g->verbose)++; break;
+				case 'p': (g->showdirs)++; break;
+				case 'd': (g->debug)++; break;
+				case 'i':
+						  if ((i < argc -1) && (argv[i+1][0] != '-')){
+							  i++;
+							  g->inputpath = strdup(argv[i]);
+						  }
+						  break;
+				default:
+						  fprintf(stderr,"Unknown parameter (%s)\n", argv[i]);
+						  exit(1);
+			}
+		}
+	}
+	return 0;
 }
 
 
 int main(int argc, char **argv) {
 
-	if (argc < 1) {
-		fprintf(stderr,"%s <path to scan>\n", argv[0]);
+	struct globals g;
+
+	glb = &g;
+	g.showdirs = 0;
+	g.debug = 0;
+	g.verbose = 0;
+
+	if (argc < 2) {
+		fprintf(stderr,"%s [-p] -i <path to scan>\n", argv[0]);
 		return 1;
 	}
 
-	nftw(argv[1], md5test, 10, FTW_MOUNT|FTW_PHYS);
+	nftw( g.inputpath, md5test, 10, FTW_MOUNT|FTW_PHYS);
 
 
 
